6fe4923d09b64d598e4a8b52b87c5b0e3048d6c7
asd-developer <andreduarte.profissional@gmail.com>
2020-12-20 03:21:02 +0000

    Added return the square kata

---
+++ Katas/8 Kyu/Return the square.js
@@ -0,0 +1,11 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+const square = (num) =>{
+    return num**2 
+  }
+
+  //OR
+
+  const square = (num) =>{
+    return num*num 
+  }
